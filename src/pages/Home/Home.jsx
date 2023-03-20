82fe4bfa2027a81c6eeb3e2132592776bd92f765
luix gabriel <luisgasbriel@gmail.com>
2023-03-20 19:37:22 -0300

    config homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import LastMatches from '../lastMatches/LastMatches';
 import homeImg from '../../assets/imgs/voleio.png';
 import './Home.css';
 
 function Home() {
   return (
-    <div className="main">
-      <div className="homepage">
-        <img src={homeImg} alt="voleio" />
-        <h1>INTERSALAS</h1>
+    <>
+      <div className="main">
+        <div className="homepage">
+          <img src={homeImg} alt="voleio" />
+          <h1>INTERSALAS</h1>
+        </div>
       </div>
-    </div>
+      <LastMatches />
+    </>
   );
 }
 
---
+++ src/pages/lastMatches/LastMatches.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './LastMatches.css';
+
+function LastMatches() {
+  return (
+    <div className="lastMatches">
+      <h1>Ultimas Partidas</h1>
+      <div className="results">
+        <h4>Arsenal 3 x 0 vasco</h4>
+        <h4>Arsenal 3 x 0 vasco</h4>
+        <h4>Arsenal 3 x 0 vasco</h4>
+        <h4>Arsenal 3 x 0 vasco</h4>
+        <h4>Arsenal 3 x 0 vasco</h4>
+      </div>
+    </div>
+  );
+}
+
+export default LastMatches;
