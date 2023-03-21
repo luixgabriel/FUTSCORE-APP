6415e6f9cde0ea5891f67c05d26955a8953b9182
luix gabriel <luisgasbriel@gmail.com>
2023-03-21 10:02:44 -0300

    Finishing homepage

--- src/pages/lastMatches/LastMatches.jsx
+++ src/pages/lastMatches/LastMatches.jsx
@@ -6,11 +6,49 @@ function LastMatches() {
     <div className="lastMatches">
       <h1>Ultimas Partidas</h1>
       <div className="results">
-        <h4>Arsenal 3 x 0 vasco</h4>
-        <h4>Arsenal 3 x 0 vasco</h4>
-        <h4>Arsenal 3 x 0 vasco</h4>
-        <h4>Arsenal 3 x 0 vasco</h4>
-        <h4>Arsenal 3 x 0 vasco</h4>
+        <div className="matchCurrent">
+          <p>EM ANDAMENTO</p>
+          <div className="game">
+            <h4 style={{ 'margin-right': '20px' }}>Arsenal</h4>
+            <span>3</span>
+            <span style={{ color: '#2596be', margin: '0 10px' }}>x</span>
+            <span>0</span>
+            <h4 style={{ 'margin-left': '20px' }}>Vasco</h4>
+          </div>
+        </div>
+
+        <div className="matchCurrent">
+          <p>EM ANDAMENTO</p>
+          <div className="game">
+            <h4 style={{ 'margin-right': '20px' }}>Arsenal</h4>
+            <span>3</span>
+            <span style={{ color: '#2596be', margin: '0 10px' }}>x</span>
+            <span>0</span>
+            <h4 style={{ 'margin-left': '20px' }}>Vasco</h4>
+          </div>
+        </div>
+
+        <div className="matchCurrent">
+          <p style={{ color: 'green' }}>FINALIZADO</p>
+          <div className="game">
+            <h4 style={{ 'margin-right': '20px' }}>Arsenal</h4>
+            <span>3</span>
+            <span style={{ color: '#2596be', margin: '0 10px' }}>x</span>
+            <span>0</span>
+            <h4 style={{ 'margin-left': '20px' }}>Vasco</h4>
+          </div>
+        </div>
+
+        <div className="matchCurrent">
+          <p>EM ANDAMENTO</p>
+          <div className="game">
+            <h4 style={{ 'margin-right': '20px' }}>Arsenal</h4>
+            <span>3</span>
+            <span style={{ color: '#2596be', margin: '0 10px' }}>x</span>
+            <span>0</span>
+            <h4 style={{ 'margin-left': '20px' }}>Vasco</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
