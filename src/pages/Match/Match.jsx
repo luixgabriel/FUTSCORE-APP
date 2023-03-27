14a2879edcfd80a6fd2655d2590d250e1fbf6be0
luix gabriel <luisgasbriel@gmail.com>
2023-03-27 17:59:29 -0300

    bug checkbox fixed?

--- src/pages/Match/Match.jsx
+++ src/pages/Match/Match.jsx
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 import Header from '../../components/Header/Header';
 import './Match.css';
 import soccerField from '../../assets/imgs/soccerStadium.png';
 import shirtTeam from '../../assets/imgs/shirtTeam.png';
+import axios from '../../services/axios';
 
+const MySwal = withReactContent(Swal);
 function Match() {
-  const [duration, setDuration] = useState(0);
+  // const [duration, setDuration] = useState(0);
   const [checkedboxes, setCheckedboxes] = useState([]);
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const getTeams = async () => {
+      const teamsAPI = await axios.get();
+      setTeams(teamsAPI.data);
+    };
+
+    getTeams();
+  }, []);
 
   const handleChange = async (e) => {
     const { value } = e.target;
     const { checked } = e.target;
+    console.log(value);
+
     if (checked) {
       setCheckedboxes([...checkedboxes, value]);
+    }
+
+    if (checkedboxes.length > 0) {
+      MySwal.fire({
+        title: <p>Ooops..</p>,
+        text: 'Só é permitido uma checkbox de duração selecionada.',
+        icon: 'error',
+        timer: 2000,
+        confirmButtonText: 'Vamos tentar novamente.',
+      });
+      setCheckedboxes([]);
+      setTimeout(() => {
+        window.location.reload();
+      }, '1500');
     }
   };
   return (
@@ -82,7 +112,7 @@ function Match() {
               </select>
             </div>
           </div>
-          <h1>hello world</h1>
+          <button type="submit">Criar partida</button>
         </div>
       </div>
     </>
